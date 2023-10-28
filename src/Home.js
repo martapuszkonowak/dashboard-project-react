920be020351f5b5aa8237caa500c2dcd37c20d66
Marta Nowak <138518482+martapuszkonowak@users.noreply.github.com>
2023-10-28 11:46:37 +0100

    Added useEffect hook and created table

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Home() {
   // todo: load clients via API axios
@@ -7,16 +7,48 @@ function Home() {
 
   const [clients, setClients] = useState([]);
 
-  const apiUrl = "http://localhost:8000/clients";
-
-  axios.get(apiUrl).then((r) => {
-    setClients(r.data);
-  });
+  useEffect(() => {
+    console.log("Within useEffect...");
+    axios
+      .get("http://localhost:8000/clients")
+      .then(function (r) {
+        console.log("API data received: %o", r.data);
+        return setClients(r.data);
+      })
+      .catch(function () {
+        console.log("API error");
+      });
+  }, []);
 
   return (
     <div className="Home">
       <p>Home</p>
-      <p>{JSON.stringify(clients)}</p>
+      <p>Data table here</p>
+      <h2>Clients' list</h2>
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>First name</th>
+          <th>Last name</th>
+          <th>Address</th>
+          <th>Email</th>
+          <th>Actions</th>
+        </tr>
+
+        {clients.map((client) => (
+          <tr>
+            <td>{client.id}</td>
+            <td>{client.first_name}</td>
+            <td>{client.last_name}</td>
+            <td>{client.address}</td>
+            <td>{client.email}</td>
+            <td>
+              <button>X</button>
+              <button>Edit</button>
+            </td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
